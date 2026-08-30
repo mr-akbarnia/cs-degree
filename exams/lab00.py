26ff007e5e5d5d32ccccb43bec59d5fd030b0afe
mr-akbarnia <17197586+mr-akbarnia@users.noreply.github.com>
2026-08-30 15:00:58 +0330

    lab00 submition

---
+++ exams/lab00.py
@@ -0,0 +1,11 @@
+"""Lab 0: Getting Started."""
+
+
+def twenty_twenty_six():
+    """Come up with the most creative expression that evaluates to 2026
+    using only numbers and the +, *, and - operators (or ** and % if you'd like).
+
+    >>> twenty_twenty_six()
+    2026
+    """
+    return 2 ** 11 - 22
